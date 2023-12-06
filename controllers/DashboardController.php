e7b1a9dc123a113a4896a654d28a20bf824c8a30
Cintia <cintiaa_utrera@outlook.es>
2023-12-06 12:58:38 -0300

    obteniendo proyectos del usuario

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -11,9 +11,13 @@ class DashboardController{
     public static function index(Router $router){
         session_start();
         isAuth();
+        $id = $_SESSION['id'];
+
+        $proyectos = Proyecto::belongsTo('propietarioId', $id);
 
         $router->render('dashboard/index', [
-            'titulo' => 'Proyectos'
+            'titulo' => 'Proyectos',
+            'proyectos' => $proyectos
         ]);
     }
 
