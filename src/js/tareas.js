d74e7c640ec33ff54a1c13c878689d1f31101975
Cintia <cintiaa_utrera@outlook.es>
2023-12-07 14:09:06 -0300

    creando modal

--- src/js/tareas.js
+++ src/js/tareas.js
@@ -4,6 +4,22 @@
     nuevaTareaBtn.addEventListener('click', mostrarFormulario);
 
     function mostrarFormulario(){
+        const modal = document.createElement('DIV');
+        modal.classList.add('modal');
+        modal.innerHTML = `
+            <form class="formulario nueva-tarea">
+                <legend>Añade una nueva tarea</legend>
+                <div class="campo">
+                    <label>Tarea</label>
+                    <input type="text" name="tarea" placeholder="Añadir tarea al proyecto" id="tarea" />
+                </div>
+                <div class="opciones">
+                    <input type="submit" class="submit-nueva-tarea" value="Añadir Tarea" />
+                    <button type="button" class="cerrar-modal">Cancelar</button>
+                </div>
+            </form>
+        `;
 
+        document.querySelector('body').appendChild(modal);
     }
 })();
