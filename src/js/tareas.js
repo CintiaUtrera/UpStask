f9227eedba4ef12497b559de0a5ef3b510bfae23
Cintia <cintiaa_utrera@outlook.es>
2023-12-07 13:59:11 -0300

    añadiendo js para mostrar el formulario

---
+++ src/js/tareas.js
@@ -0,0 +1,9 @@
+(function() {
+    // Boton para mostrar el modal de agregar tareas 
+    const nuevaTareaBtn = document.querySelector('#agregar-tarea');
+    nuevaTareaBtn.addEventListener('click', mostrarFormulario);
+
+    function mostrarFormulario(){
+
+    }
+})();
--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -7,3 +7,11 @@
 </div>
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
+
+<?php
+
+$script = '
+    <script src="build/js/tareas.js"></script>
+'
+
+?>
