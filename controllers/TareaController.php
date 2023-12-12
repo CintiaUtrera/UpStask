be6ddf7eebe641b13e89c109dc3df692ef811074
Cintia <cintiaa_utrera@outlook.es>
2023-12-12 11:50:23 -0300

    creando routing

---
+++ controllers/TareaController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Controllers;
+
+
+class TareaController{
+    public static function index(){
+
+    }
+
+
+    public static function crear(){
+        if($_SERVER['REQUEST_MEETHOD'] === 'POST'){
+
+        }
+    }
+
+
+    public static function actualizar(){
+        if($_SERVER['REQUEST_MEETHOD'] === 'POST'){
+            
+        }
+    }
+    
+
+
+    public static function eliminar(){
+        if($_SERVER['REQUEST_MEETHOD'] === 'POST'){
+            
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use MVC\Router;
 
 use Controllers\LoginController;
 use Controllers\DashboardController;
+use Controllers\TareaController;
+
 $router = new Router();
 
 // Login
@@ -37,7 +39,10 @@ $router->get('/proyecto', [DashboardController::class, 'proyecto']);
 $router->get('/perfil', [DashboardController::class, 'perfil']);
 
 // Api para las tareas
-
+$router->get('/api/tareas', [TareaController::class, 'index']);
+$router->post('/api/tarea', [TareaController::class, 'crear']);
+$router->post('/api/tarea/actualizar', [TareaController::class, 'actualizar']);
+$router->post('/api/tarea/eliminar', [TareaController::class, 'eliminar']);
 
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
