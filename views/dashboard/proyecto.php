3829e37271c48046d4b56f9b73c5dd0700eefc8b
Cintia <cintiaa_utrera@outlook.es>
2023-12-13 12:56:29 -0300

    mostrando tareas

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -4,6 +4,7 @@
     <div class="contenedor-nueva-tarea">
         <button class="agregar-tarea" type="button" id="agregar-tarea">&#43; Nueva Tarea</button>
     </div>
+    <ul id="listado-tareas" class="listado-tareas"></ul>
 </div>
 
 <?php include_once __DIR__ . '/footer-dashboard.php'; ?>
