61203fe0eb92b37862d32df4a569c51143ad0190
Cintia <cintiaa_utrera@outlook.es>
2023-12-13 12:28:37 -0300

    creando api para mostrar las tareas

--- controllers/TareaController.php
+++ controllers/TareaController.php
@@ -7,7 +7,19 @@ use Model\Proyecto;
 
 class TareaController{
     public static function index(){
+        
+        $proyectoId = $_GET['id'];
 
+        if(!$proyectoId) header('Location: /dashboard');
+
+        $proyecto = Proyecto::where('url', $proyectoId);
+        session_start();
+        if(!$proyecto || $proyecto->propietarioId !== $_SESSION['id']) header('Location: /404');
+        
+        $tareas = Tarea::belongsTo('proyectoId', $proyecto->id);
+        echo json_encode(['tareas' => $tareas]);
+    
+    
     }
 
 
