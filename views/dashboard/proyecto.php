f830fc0c5f5bf166670463da4d046bc84f9e4939
Cintia <cintiaa_utrera@outlook.es>
2023-12-18 19:38:14 -0300

    css al formulario

--- views/dashboard/proyecto.php
+++ views/dashboard/proyecto.php
@@ -4,6 +4,25 @@
     <div class="contenedor-nueva-tarea">
         <button class="agregar-tarea" type="button" id="agregar-tarea">&#43; Nueva Tarea</button>
     </div>
+    <div id="filtros" class="filtros">
+        <div class="filtros-input">
+            <h3>Filtros:</h3>
+            <div class="campo">
+                <label for="todas">Todas</label>
+                <input type="radio" id="todas" name="filtro" value="" checked  />
+            </div>
+
+            <div class="campo">
+                <label for="completadas">Completadas</label>
+                <input type="radio" id="completadas" name="filtro" value="1"  />
+            </div>
+
+            <div class="campo">
+                <label for="pendientes">Pendientes</label>
+                <input type="radio" id="pendientes" name="filtro" value="0"  />
+            </div>
+        </div>
+    </div>
     <ul id="listado-tareas" class="listado-tareas"></ul>
 </div>
 
